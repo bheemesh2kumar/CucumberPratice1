c774bb72156d337e53cbd8f5fb1363d083427d12
bheemesh <manu.bheemesh@gmail.com>
2023-02-15 16:33:02 +0530

    google search page locators

---
+++ src/main/java/locators/GoogleSearchLocators.java
@@ -0,0 +1,10 @@
+package locators;
+
+public class GoogleSearchLocators {
+
+
+    public static final String AutoSuggoogleSearch = "//div[@class='erkvQe']/div/ul//li//descendant::div[@class='wM6W7d']/span";
+
+    public static final String InputBoxgoogleSearch = "div.a4bIc>input";
+
+}
